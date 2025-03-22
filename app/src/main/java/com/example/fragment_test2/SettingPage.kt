d8a5f1a5ae6f17cc13db09d298771398b2dfd1bf
halina2222 <125880072+halina2222@users.noreply.github.com>
2025-03-23 01:35:16 +0800

    setting page add icon

--- app/src/main/java/com/example/fragment_test2/SettingPage.kt
+++ app/src/main/java/com/example/fragment_test2/SettingPage.kt
@@ -1,15 +1,21 @@
 package com.example.fragment_test2
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
@@ -22,12 +28,37 @@ fun SettingPage(){
         Font(R.font.inknutantiqualight)
     )
     Column(){
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically)
+        {
+            Spacer(modifier = Modifier.width(8.dp))
+            Image(
+                painter = painterResource(id = R.drawable.usericon), // Replace with your drawable resource ID
+                contentDescription = "Circular Image",
+                modifier = Modifier
+                    .size(80.dp)     // Set the desired size
+                    .clip(CircleShape) // Apply the circle shape clip
+            )
+            Spacer(modifier = Modifier.width(15.dp))
+            Text(
+                text = "  User Name",
+                color = setting_page_font,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                fontFamily = fontFamily,
+                modifier = Modifier.wrapContentWidth()
+            )
+
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Divider(color = Color.Black, thickness = 2.dp, modifier = Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = "  Avatar Management  ",
             color = setting_page_font,
             fontSize = 20.sp,
             textDecoration = TextDecoration.Underline,
-            textAlign = TextAlign.Start,
+            textAlign = TextAlign.Center,
             fontFamily = fontFamily,
             modifier = Modifier.fillMaxWidth()
         )
@@ -43,7 +74,7 @@ fun SettingPage(){
             color = setting_page_font,
             fontSize = 20.sp,
             textDecoration = TextDecoration.Underline,
-            textAlign = TextAlign.Start,
+            textAlign = TextAlign.Center,
             fontFamily = fontFamily,
             modifier = Modifier.fillMaxWidth()
         )
